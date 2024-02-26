4cff524e04b24dc2daf94e104f1581ccb86549d3
Desquivel501 <3523114870101@ingenieria.usac.edu.gt>
2024-02-26 17:23:27 -0600

    Se arreglo el logo en el navbar

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -3,22 +3,22 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
-import logo from '../../assets/usac_logo_white.svg';
+import logo from '../../assets/market_logo_white.png';
 import { Cart } from 'react-bootstrap-icons';
 
 function CustomNavbar() {
   return (
     <Navbar bg="dark" data-bs-theme="dark" fixed='top'>
         <Container>
-            <Navbar.Brand href="#home">
+            <Navbar.Brand href="">
                 <img
                 alt=""
                 src={logo}
-                width="30"
+                width="auto"
                 height="30"
                 className="d-inline-block align-top"
                 />{' '}
-                MarketUSAC
+                
             </Navbar.Brand>
             <Nav className="me-auto">
                 <Nav.Link href="">Home</Nav.Link>
