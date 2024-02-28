fa1af123847f2a320b7eae804d67beba59ae0f71
Sebastián Alejandro de León Tenaz <63890439+sebbbasdl@users.noreply.github.com>
2024-02-27 19:03:55 -0600

    Perfiles terminados (Cliente, Vendedor, Administrador)

--- frontend/src/utilities/routes.jsx
+++ frontend/src/utilities/routes.jsx
@@ -2,6 +2,9 @@ import ForgetView from '../views/ForgettView/ForgetView';
 import LandingView from '../views/LandingView/LandingView';
 import LoginView from '../views/LoginView/LoginView';
 import RegisterRepartidorView from '../views/RegisterRepatidorView/RegisterRepatidorView';
+import ProfileSalePerson from "../components/Profiles/ProfileSalePerson";
+import ProfileUser from "../components/Profiles/ProfileUser";
+import ProfileAdmin from "../components/Profiles/ProfileAdmin";
 
 export const rutas = [
     {
@@ -23,5 +26,17 @@ export const rutas = [
     {
         path: "/forget-password",
         element: <ForgetView />
+    },
+    {
+        path:"/profile-user",
+        element:<ProfileUser/>
+    },
+    {
+        path:"/profile-sale-person",
+        element:<ProfileSalePerson/>
+    },
+    {
+        path:"/profile-admin",
+        element:<ProfileAdmin/>
     }
 ]
